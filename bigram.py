d29290178f7f55af8ef991b91b3e29a6ace66431
SoheilFM <soheil.farid1999@yahoo.com>
2025-01-24 03:51:51 +0330

    `Added training loop to bigram.py`

--- bigram.py
+++ bigram.py
@@ -104,3 +104,20 @@ m = model.to(device)
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+
+for iter in range(max_iters):
+
+    # Every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
