26823a876bee66b2daa798f74bf4dfcd5610402c
SoheilFM <soheil.farid1999@yahoo.com>
2025-01-24 18:43:37 +0330

    `Added residual connections to Block class and modified MultiHeadAttention and FeedForward classes`

--- bigram.py
+++ bigram.py
@@ -91,8 +91,8 @@ class Block(nn.Module):
         self.sa= MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
     def forward(self, x):
-        x = self.sa(x)
-        x = self.ffwd(x)
+        x = x + self.sa(x)
+        x = x +  self.ffwd(x)
         return x
 
 # Multiple Head of Self Attention in Parallels.
@@ -101,16 +101,22 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self .proj = nn.Linear(n_embd, n_embd)
+        
         
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return  out 
 # A simple Linear Layer 
 class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net= nn.Sequential(
             nn.Linear(n_embd, n_embd),
-            nn.ReLU(),)
+            nn.ReLU(),
+            nn.Linear(n_embd, n_embd) # projection Layer
+            )
     def forward(self, x):
         return self.net(x)
     
