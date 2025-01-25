4f470d5ce1bc37b24230fb08dc605f6e13dd220b
SoheilFM <soheil.farid1999@yahoo.com>
2025-01-25 20:10:50 +0330

    `Added print statement to display model And JupyterNotebook Added`

--- bigram.py
+++ bigram.py
@@ -185,7 +185,8 @@ class BigramLanguageModel(nn.Module):
     
 model = BigramLanguageModel()
 m = model.to(device)
-
+# Print number of parameters
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters') # 10 m parameters
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
